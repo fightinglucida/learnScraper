38b3338573fcda84d81c733e39e43092dc7dd481
Lucida <lucida1607@gmail.com>
2024-05-31 17:53:11 +0800

    baidu test

---
+++ 13.baidu.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+import requests
+from lxml import etree
+
+class Tieba(object):
+
+    def run(self):
+        # url
+        # headers
+        # 发送请求，获取响应
+        # 从响应中提取数据（数据和翻页用的url)
+        #
