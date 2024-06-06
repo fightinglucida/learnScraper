6ac282c39167b9eaa62489eaa83c5f7cc82e13f9
Lucida <lucida1607@gmail.com>
2024-06-06 20:10:28 +0800

    V1.0 hupu url post scraper finished

--- hupu_shuma.py
+++ hupu_shuma.py
@@ -2,11 +2,14 @@
 import time
 import random
 import requests
+import pandas as pd
 from lxml import etree
+from datetime import datetime
 
 class Hupu(object):
     def __init__(self):
         self.url = "https://bbs.hupu.com/73-postdate"
+        self.topic = ""
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
         }
@@ -17,8 +20,30 @@ class Hupu(object):
         return response.content
     def parse_list_data(self,data):
         html = etree.HTML(data.decode())
-        el_list = html.xpath("//div[@class='bbs-sl-web-post-layout']")
         data_list = []
+
+        # 保存话题定义
+        if self.topic == "":
+            post_topic = html.xpath("//head/title/text()")
+            self.topic = post_topic[0].split(" - ")[0]
+            print(self.topic)
+
+        # 统计页码总数
+        page_count_list = html.xpath("//ul[@class='hupu-rc-pagination']/li[last()-1]/a/text()")
+        page_count = int(page_count_list[0])
+        if self.page_num <= page_count:
+            self.page_num += 1
+            next_url = self.url + '-' + str(self.page_num)
+        else:
+            if page_count == 0:
+                next_url = self.url + '-' + str(self.page_num)
+                return data_list, next_url
+            else:
+                self.page_num = 0
+                return [None, None]
+
+        # 解析每一条帖子的数据
+        el_list = html.xpath("//div[@class='bbs-sl-web-post-layout']")
         for el in el_list:
             temp = {}
             parse_title = el.xpath("./div[@class='post-title']/a/text()")
@@ -26,7 +51,10 @@ class Hupu(object):
             parse_link = el.xpath("./div[@class='post-title']/a/@href")
             temp["link"] = "https://bbs.hupu.com" + parse_link[0]
             parse_reply_view = el.xpath("./div[@class='post-datum']/text()")
-            temp["reply"] = parse_reply_view[0]
+            parse_reply, parse_view = parse_reply_view[0].split(' / ')
+            temp["reply"] = parse_reply
+            temp["view"] = parse_view
+
             parse_author = el.xpath("./div[@class='post-auth']/a/text()")
             temp["author_name"] = parse_author[0]
             parse_author_link = el.xpath("./div[@class='post-auth']/a/@href")
@@ -34,31 +62,22 @@ class Hupu(object):
             parse_post_time = el.xpath("./div[@class='post-time']/text()")
             temp["post_time"] = parse_post_time[0]
             data_list.append(temp)
+        # 抓取的内容列表数量
+        post_list_count = len(el_list)
 
-        # el_list = html.xpath("//div[@class='post-title']/a")
-        # data_list = []
-        # for el in el_list:
-        #     temp = {}
-        #     parse_title = el.xpath("./text()")
-        #     temp["title"] = parse_title[0]
-        #     parse_link = el.xpath("./@href")
-        #     temp["link"] = "https://bbs.hupu.com" + parse_link[0]
-        #     data_list.append(temp)
-
-
-        page_count_list = html.xpath("//ul[@class='hupu-rc-pagination']/li[last()-1]/a/text()")
-
-        page_count = int(page_count_list[0])
-        print("当前总页码为："+ str(page_count_list))
-        if self.page_num <= page_count:
-            print(self.page_num)
-            self.page_num += 1
-        else:
-            self.page_num = 0
-            return [None,None]
-        next_url = self.url + '-' + str(self.page_num)
-
+        # 总结性打印数据
+        print(f"下一页：{self.page_num}\n总页码：{page_count_list}\n当前页抓取数据数量：{post_list_count}\n", )
         return data_list,next_url
+
+    def saveData(self, data):
+        # 获取当前时间
+        current_time = datetime.now()
+        # 格式化时间为指定格式
+        formatted_time = current_time.strftime("%Y%m%d%H%M")
+        filename = self.topic + "_" + formatted_time + ".xlsx"
+        print(filename)
+        df = pd.DataFrame(data)
+        df.to_excel(filename, index=False, engine='openpyxl')
 
     def run(self):
         next_url = self.url
@@ -77,8 +96,8 @@ class Hupu(object):
 
             self.data_list += data_list
             time.sleep(random.randint(400, 900)/1000.0)
-
-        print(self.data_list)
+        self.saveData(self.data_list)
+        print(f"总计爬取并保存数据 {len(self.data_list)} 条")
 
 if __name__ == '__main__':
     hupu = Hupu()
